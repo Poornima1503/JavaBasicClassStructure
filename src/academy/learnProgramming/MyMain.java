a3a41b26530463bf1f6a7f86ef1a4393900733e0
poornima S <poornii87@gmail.com>
2019-08-10 17:41:26 +0100

    BasicClassStructure Commit

---
+++ src/academy/learnProgramming/MyMain.java
@@ -0,0 +1,15 @@
+package academy.learnProgramming;
+
+public class MyMain {
+    public static void main(String[] args) {
+
+        Person objectPerson1 = new Person();
+        Person objectPerson2 = new Person();
+        Person objectPerson3 = new Person();
+        Person objectPerson4 = new Person();
+        Person objectPerson5 = new Person();
+        Person objectPerson6 = new Person();
+
+
+    }
+}
